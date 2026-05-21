4aba3defde2ed8e1ce6af1827e0b0aef26dd8469
leadepre <leandrodepretto16@gmail.com>
2026-05-20 21:10:29 -0300

    Resuelvo conflicto en Models Producto.php

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,25 +8,10 @@ class Producto extends Model
 {
     protected $fillable = [
         'nombre',
-<<<<<<< HEAD
         'categoria',
         'descripcion',
         'precio',
         'stock',
         'activo',
-    ];
-=======
-        'descripcion',
-        'precio',
-        'stock',
-        'url_imagen',
-        'activo',
-        ];
-protected $casts = [
-        'precio' => 'decimal:2',
-        'stock' => 'integer',
-        'activo' => 'boolean',
-        ];
-
->>>>>>> bc2483854b38680d731d03e84f671f683e59de0f
+];
 }
